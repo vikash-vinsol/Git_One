ba771a247add533e064ad667cf974082ecc75023
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-11 13:10:24 +0530

    Remove Warning method

--- Git_One/ViewController.m
+++ Git_One/ViewController.m
@@ -23,9 +23,5 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
