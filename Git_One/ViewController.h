a8717c8e3a613a447941dd3cc857c09e4f285742
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-11 12:50:27 +0530

    Add Log for successful app run

---
+++ Git_One/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Git_One
+//
+//  Created by Vikash Soni on 11/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
---
+++ Git_One/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Git_One
+//
+//  Created by Vikash Soni on 11/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"App run complete");
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
