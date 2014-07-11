ce406791dad0d3de17e4d17e532f979747f9da38
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-11 12:54:43 +0530

    Add Product Class

---
+++ Git_One/Products.h
@@ -0,0 +1,13 @@
+//
+//  Products.h
+//  Git_One
+//
+//  Created by Vikash Soni on 11/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Products : NSObject
+
+@end
---
+++ Git_One/Products.m
@@ -0,0 +1,13 @@
+//
+//  Products.m
+//  Git_One
+//
+//  Created by Vikash Soni on 11/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import "Products.h"
+
+@implementation Products
+
+@end
